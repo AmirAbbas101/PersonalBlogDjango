79968807757124fd8a59bcde0db461b04338dd2e
AmirAbbas101 <shaikh4851@gmail.com>
2025-01-20 12:08:39 +0500

    feat: integrate home app into the project

--- PersonalBlogDjango/urls.py
+++ PersonalBlogDjango/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path("", include('home.urls')),
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
